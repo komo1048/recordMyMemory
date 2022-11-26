f0316ed4811083fded600f7edf6c68ce39eebc1c
DESKTOP-EDMI1HM\SH <komo1048@gmail.com>
2022-11-26 22:16:14 +0900

    router 적용해보기

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import Card from "./components/card/Card";
 import classes from "./components/container/Container.module.css";
 import Modal from "react-modal";
 import { useCallback, useEffect, useState } from "react";
+import Header from "./components/header/Header";
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/login/Login.js";
 
 function App() {
   const [memoryList, setMemoryList] = useState([]);
 
   const fetchMemory = useCallback(async () => {
     console.log("fetchMemory Running");
-    const response = await fetch(
-      "https://react-http-38d3b-default-rtdb.firebaseio.com/memory.json"
-    );
+    const response = await fetch("https://react-http-38d3b-default-rtdb.firebaseio.com/memory.json");
     const data = await response.json();
 
     const loadedMemory = [];
@@ -29,18 +30,15 @@ function App() {
     setMemoryList([...loadedMemory]);
   }, []);
 
-  const onAddMemory = async (item) => {
+  const onAddMemory = async item => {
     console.log("onAddMemory Running");
-    await fetch(
-      "https://react-http-38d3b-default-rtdb.firebaseio.com/memory.json",
-      {
-        body: JSON.stringify(item),
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlcoded",
-        },
-      }
-    );
+    await fetch("https://react-http-38d3b-default-rtdb.firebaseio.com/memory.json", {
+      body: JSON.stringify(item),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlcoded",
+      },
+    });
 
     fetchMemory();
   };
@@ -49,20 +47,17 @@ function App() {
     fetchMemory();
   }, [fetchMemory]);
 
-  const onDeleteMemory = async (memoryId) => {
+  const onDeleteMemory = async memoryId => {
     console.log(memoryId);
 
-    await fetch(
-      `https://react-http-38d3b-default-rtdb.firebaseio.com/memory/${memoryId}.json`,
-      {
-        method: "DELETE",
-      }
-    );
+    await fetch(`https://react-http-38d3b-default-rtdb.firebaseio.com/memory/${memoryId}.json`, {
+      method: "DELETE",
+    });
 
     fetchMemory();
   };
 
-  const onUpdateMemory = async (memory) => {
+  const onUpdateMemory = async memory => {
     const memoryBody = {
       title: memory.title,
       date: memory.date,
@@ -72,33 +67,34 @@ function App() {
       memoryBody.image = memory.image;
     }
 
-    await fetch(
-      `https://react-http-38d3b-default-rtdb.firebaseio.com/memory/${memory.id}.json`,
-      {
-        method: "PATCH",
-        body: JSON.stringify(memoryBody),
-      }
-    );
+    await fetch(`https://react-http-38d3b-default-rtdb.firebaseio.com/memory/${memory.id}.json`, {
+      method: "PATCH",
+      body: JSON.stringify(memoryBody),
+    });
 
     fetchMemory();
   };
 
   return (
-    <div className={classes.box}>
-      <CreateButton onAddMemory={onAddMemory} />
-      <div className={classes.container}>
-        {memoryList.map((memory) => {
-          return (
-            <Card
-              key={memory.id}
-              memory={memory}
-              deleteMemory={onDeleteMemory}
-              upDateMemory={onUpdateMemory}
-            />
-          );
-        })}
-      </div>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Login to="/" />} />
+        <Route
+          path="/main"
+          element={
+            <div className={classes.box}>
+              <CreateButton onAddMemory={onAddMemory} />
+              <div className={classes.container}>
+                {memoryList.map(memory => {
+                  return <Card key={memory.id} memory={memory} deleteMemory={onDeleteMemory} upDateMemory={onUpdateMemory} />;
+                })}
+              </div>
+            </div>
+          }
+        />
+      </Routes>
+    </>
   );
 }
 Modal.setAppElement("#root");
---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      {/* <Routes>
+        <Route path="/" element={<Navigate to="/" />} />
+        <Route paht="/main" element={<Navigate to="/main" />} />
+      </Routes> */}
+      헤더입니다
+    </div>
+  );
+};
+
+export default Header;
